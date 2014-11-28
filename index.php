edb48ee9893b4956a4c29764a4d9ef0a06c00773
wangchaojie <popxmz@Gmail.com>
2014-11-28 14:21:11 +0800

    测试增删改，git的显示方法

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 	phpinfo();
-	echo "1st Line Here";
+	echo "1sts Line Here";
 	echo "2nd Line Here";
-	echo "3rd Line Here trully on master branch";
 	echo "Ready to Stash";
 	echo "Stash execize OK";
+	echo "a";
 ?>
